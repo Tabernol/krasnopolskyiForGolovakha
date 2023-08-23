3bff797e384430b629b87ddd20ef688365db7953
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2023-08-23 21:52:03 +0300

    Initial commit Added table my_number

---
+++ src/main/java/com/krasnopolskyi/krasnopolskyiforgolovakha/KrasnopolskyiForGolovakhaApplication.java
@@ -0,0 +1,13 @@
+package com.krasnopolskyi.krasnopolskyiforgolovakha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrasnopolskyiForGolovakhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrasnopolskyiForGolovakhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasnopolskyi/krasnopolskyiforgolovakha/KrasnopolskyiForGolovakhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnopolskyi.krasnopolskyiforgolovakha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrasnopolskyiForGolovakhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
